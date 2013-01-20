c5fbef62735eeef8d12428ddbd4a0d79904b7385
Tommy Marshall <tommy.marshall@viget.com>
2013-01-20 16:34:49 -0500

    Refactored a bit. Updated ignore folders in htaccess.

--- assets/javascripts/imaginari.js
+++ assets/javascripts/imaginari.js
@@ -2,7 +2,7 @@ var Imaginari = (function (){
 	var breakpoints = [2400, 1800, 1600, 1400, 980, 768, 480]; // High to low
 	var currentBreakpoint = -1;
 	var screenWidth;
-	var pixelRatio = ("devicePixelRatio" in window ? "," +devicePixelRatio : ",1");
+	var pixelRatio = ( 'devicePixelRatio' in window ? ',' + devicePixelRatio : ',1' );
 
 	init = function () {
 		this.binds();
@@ -10,13 +10,13 @@ var Imaginari = (function (){
 	},
 
 	binds = function() {
-		window.addEventListener('resize', this.monitor, false);
+		window.addEventListener( 'resize', this.monitor, false );
 	},
 
 	monitor = function() {
 		this.getScreenWidth();
 
-		if ( this.screenWidth !== this.currentBreakpoint) {
+		if ( this.screenWidth !== this.currentBreakpoint ) {
 			this.currentBreakpoint = this.screenWidth;
 
 			this.setCookie();
@@ -25,23 +25,25 @@ var Imaginari = (function (){
 	},
 
 	updateImages = function() {
-		var images = document.getElementsByTagName("img");
+		var images = document.getElementsByTagName( 'img' );
 
-		for (var i = 0; i < images.length; i++) {
-			images[i].src = images[i].src.substring(0, (images[i].src.indexOf('?') > 0 ? images[i].src.indexOf('?') : images[i].src.length)) + "?" + this.screenWidth;
+		for ( var i = 0; i < images.length; i++ ) {
+			if ( images[i].src.indexOf( 'assets' ) === -1 ) {
+				images[i].src = images[i].src.substring( 0, ( images[i].src.indexOf('?') > 0 ? images[i].src.indexOf( '?' ) : images[i].src.length ) ) + '?' + this.screenWidth;
+			}
 		}
 	},
 
 	getScreenWidth = function() {
-		for (var i = 0; i < breakpoints.length; i++) {
-			if (window.innerWidth < breakpoints[i]) {
+		for ( var i = 0; i < breakpoints.length; i++ ) {
+			if ( window.innerWidth < breakpoints[i] ) {
 				this.screenWidth = breakpoints[i];
 			}
 		}
 	},
 
 	setCookie = function() {
-		document.cookie = "resolution=" + this.screenWidth + pixelRatio +'; path=/';
+		document.cookie = 'resolution=' + this.screenWidth + pixelRatio +'; path=/';
 	};
 
 	return this.init();
